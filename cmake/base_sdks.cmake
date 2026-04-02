f1c1fa755e8dcef162430b988d85f70935bd7d55
shockham <shockham@protonmail.com>
2026-04-02 12:12:06 +0100

    try win32 executable flag

--- cmake/base_sdks.cmake
+++ cmake/base_sdks.cmake
@@ -190,7 +190,7 @@ function(guarantee_vst3sdk)
 
     if (NOT TARGET vst3_validator)
         if(MINGW)
-            set(MINGW_VALIDATOR_FLAG -DCMAKE_CXX_FLAGS="-mwindows -municode")
+            set(MINGW_VALIDATOR_FLAG -DCMAKE_WIN32_EXECUTABLE=ON)
         endif(MINGW)
 
         add_custom_target(vst3_validator)
